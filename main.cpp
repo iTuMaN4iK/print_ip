0f3b1b5585aff0272ed2e8598dd06f6e9d7971bd
iTuMaN4iK <swaga.ituman4ik@gmail.com>
2019-10-16 22:30:18 +0500

    Пошаманил с doxygen'ом

--- main.cpp
+++ main.cpp
@@ -1,28 +1,50 @@
-#include "lib.h"
+/**
+ * @file main.cpp
+ * @author PerminovDV (swaga.ituman4ik@gmail.com)
+ * @brief Программа печати условного ip адреса
+ * @version 0.1
+ * @date 2019-10-16
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include "include.h"
+#include "lib.h"
 #include <iostream>
-
-template <class T>
-void print(T && val){
-    std::cout<<print_ip<T>(val)<<std::endl;
+/**
+ * @brief Функцию печати условного ip-адреса 
+ * 
+ * @tparam T 
+ * @param val 
+ */
+template <class T> void print(T &&val) {
+  std::cout << print_ip<T>(val) << std::endl;
 }
-template <class T>
-void print(T & val){
-    std::cout<<print_ip<T>(val)<<std::endl;
+/**
+ * @brief Функцию печати условного ip-адреса
+ * 
+ * @tparam T 
+ * @param val 
+ */
+template <class T> void print(T &val) {
+  std::cout << print_ip<T>(val) << std::endl;
 }
-int main (int, char **)
-{
-    std::cout << "Version: " << version() << std::endl;
-    print(char(-1));
-    print(short(0));
-    print(int(2130706433));
-    print(long(8875824491850138409));
-    print(std::string("127.0.0.1"));
-    print(std::list<int>{2130706433,2130706433});
-    print(std::list<short>{0,0});
-    print(std::vector<int>{2130706433,2130706433});
-    print(std::vector<short>{0,0});
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int main(int, char **) {
+  std::cout << "Version: " << version() << std::endl;
+  print(char(-1));
+  print(short(0));
+  print(int(2130706433));
+  print(long(8875824491850138409));
+  print(std::string("127.0.0.1"));
+  print(std::list<int>{2130706433, 2130706433});
+  print(std::list<short>{0, 0});
+  print(std::vector<int>{2130706433, 2130706433});
+  print(std::vector<short>{0, 0});
 
-    return 0;
-
+  return 0;
 }
--- test_printIP.cpp
+++ test_printIP.cpp
@@ -1,31 +1,30 @@
 #include "include.h"
 #include <gtest/gtest.h>
 
-TEST(printIP,typeChar){
-    EXPECT_EQ(print_ip<char>(char(-1)),"255");
-    EXPECT_EQ(print_ip<char>(char(-2)),"254");
+TEST(printIP, typeChar) {
+  EXPECT_EQ(print_ip<char>(char(-1)), "255");
+  EXPECT_EQ(print_ip<char>(char(-2)), "254");
 }
-TEST(printIP,typeShor){
-    EXPECT_EQ(print_ip<short>(short(0)),"0.0");
+TEST(printIP, typeShor) { EXPECT_EQ(print_ip<short>(short(0)), "0.0"); }
+TEST(printIP, typeInt) {
+  EXPECT_EQ(print_ip<int>(int(2130706433)), "127.0.0.1");
 }
-TEST(printIP,typeInt){
-    EXPECT_EQ(print_ip<int>(int(2130706433)),"127.0.0.1");
+TEST(printIP, typeLong) {
+  EXPECT_EQ(print_ip<long>(long(8875824491850138409)),
+            "123.45.67.89.101.112.131.41");
 }
-TEST(printIP,typeLong){
-    EXPECT_EQ(print_ip<long>(long(8875824491850138409)),"123.45.67.89.101.112.131.41");
+TEST(printIP, typeString) {
+  EXPECT_EQ(print_ip<std::string>(std::string("127.0.0.1")), "127.0.0.1");
 }
-TEST(printIP,typeString){
-    EXPECT_EQ(print_ip<std::string>(std::string("127.0.0.1")),"127.0.0.1");
+TEST(printIP, typeList) {
+  std::list<int> li{2130706433, 2130706433};
+  EXPECT_EQ(print_ip<std::list<int>>(li), "127.0.0.1..127.0.0.1");
+  std::list<short> ls{0, 0};
+  EXPECT_EQ(print_ip<std::list<short>>(ls), "0.0..0.0");
 }
-TEST(printIP,typeList){
-    std::list<int> li{2130706433,2130706433};
-    EXPECT_EQ(print_ip<std::list<int>>(li),"127.0.0.1..127.0.0.1");
-    std::list<short> ls{0,0};    
-    EXPECT_EQ(print_ip<std::list<short>>(ls),"0.0..0.0");
-}
-TEST(printIP,typeVector){
-    std::vector<int> li{2130706433,2130706433};
-    EXPECT_EQ(print_ip<std::vector<int>>(li),"127.0.0.1..127.0.0.1");
-    std::vector<short> ls{0,0};    
-    EXPECT_EQ(print_ip<std::vector<short>>(ls),"0.0..0.0");
+TEST(printIP, typeVector) {
+  std::vector<int> li{2130706433, 2130706433};
+  EXPECT_EQ(print_ip<std::vector<int>>(li), "127.0.0.1..127.0.0.1");
+  std::vector<short> ls{0, 0};
+  EXPECT_EQ(print_ip<std::vector<short>>(ls), "0.0..0.0");
 }
