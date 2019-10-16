dfb3d38ec929a7c4d7ca40987e77d5404494859b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-16 17:52:19 +0000

    Deploy iTuMaN4iK/print_ip to github.com/iTuMaN4iK/print_ip.git:gh-pages

---
+++ CMakeFiles/test_printIP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_printIP.dir/test_printIP.cpp.o"
+  "test_printIP.pdb"
+  "test_printIP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_printIP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/travis/build/iTuMaN4iK/print_ip
+# Build directory: /home/travis/build/iTuMaN4iK/print_ip
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_printIP "test_printIP")
